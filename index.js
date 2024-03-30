2b31943b99d402c4e6d77fb78ee42ab49504cacd
Sumit pal <tonystark83033@gmail.com>
2024-03-31 02:31:50 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const httpProxy = require('http-proxy');
+const app = express();
+const PORT  = 8000;
+
+const BASE_PATH = "https://deployify-project.s3.ap-south-1.amazonaws.com/__output";
+
+const proxy = httpProxy.createProxy();
+app.use((req,res)=>{
+    const hostname = req.hostname;
+    const subdomain = hostname.split('.')[0];
+
+    const resolvesTo = `${BASE_PATH}/${subdomain}`;
+
+   return proxy.web(req,res,{target:resolvesTo , changeOrigin: true});
+})
+
+app.get("/health",(req,res)=>{
+    return res.send("ok");
+})
+app.get('/',(res,req)=>{
+    return res.send("welcome to deployify");
+})
+
+proxy.on('proxyReq',(proxyReq,req,res)=>{
+    const url = req.url;
+    if(url === '/'){
+        proxyReq.path +='index.html';
+        
+    }
+    
+})
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
