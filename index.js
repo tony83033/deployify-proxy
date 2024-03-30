155e87e483eea5ad490c532b257569a581319bf2
Sumit pal <tonystark83033@gmail.com>
2024-03-31 02:38:33 +0530

    fix test route add /index.html

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/sumit",(req,res)=>{
     // const hostname = req.hostname;
     // const subdomain = hostname.split('.')[0];
 
-    const resolvesTo = `https://deployify-project.s3.ap-south-1.amazonaws.com/__output/vite2`;
+    const resolvesTo = `https://deployify-project.s3.ap-south-1.amazonaws.com/__output/vite2/index.html`;
 
    return proxy.web(req,res,{target:resolvesTo , changeOrigin: true});
 
